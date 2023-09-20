9ef33c4317027a6ab0e9625b1575876fb9f562e0
saveriomercante <saveriomercante@gmail.com>
2023-09-20 21:31:41 +0200

    Add Fixings
    Fixing Try and Catch , adding constructor, reinforcing encapsulation

--- Giocatore.java
+++ Giocatore.java
@@ -3,79 +3,49 @@ import java.util.Scanner;
 
 public class Giocatore {
 
-    Scanner input = new Scanner(System.in);
 
     private String nomeGiocatore; 
     private int ficheIniziali; 
     private int fichesPuntata;
 
-    public void setNomeGiocatore(String nomeGiocatore) {
-        boolean ancora = true;
-        do {
-            if(nomeGiocatore.length() > 1) {
-                this.nomeGiocatore = nomeGiocatore;
-                ancora = false;
-            } else {
-                System.out.println("Il nome deve essere di almeno 2 caratteri, prego ridigitare.");
-                nomeGiocatore = input.nextLine();
-            }
-        } while(ancora);
-    }
 
-    public String getNomeGiocatore() {
-        return nomeGiocatore;
-    }
-
-    public void setFicheIniziali(int ficheIniziali) {
-        boolean ancora = true;
-        do {
-            if (ficheIniziali > 0) {
-                this.ficheIniziali = ficheIniziali;
-                ancora = false;
-            } else {
-                try {
-                    System.out.println("Digita il numero di fiche iniziali corretto, maggiore di zero.");
-                    ficheIniziali = input.nextInt();
-                } catch (InputMismatchException exc) {
-                    System.out.println("Input immesso non compatibile, reinserisci l'importo da convertire in fiches iniziali.");
-                    input.nextLine(); // Consuma l'input errato
-                    
-                }
-            }
-        } while (ancora);
-    }
-
-    public int getFicheIniziali() {
-        return ficheIniziali;
-    }
-
-    public void setFichesPuntata(int fichesPuntata) {
-
-        boolean ancora = true;
-        do {
-            if(fichesPuntata > 0) {
-                this.fichesPuntata = fichesPuntata;
-                ancora = false;
-            } else {
-                try {
-                System.out.println("Devi per forza puntare almeno una fiche.");
-                fichesPuntata = input.nextInt();
-            } catch (InputMismatchException exc) {
-                System.out.println("Input immesso non compatibile, reinserisci l'importo da puntare.");
-                input.nextLine(); // Consuma l'input errato
-                fichesPuntata = input.nextInt();
-            }
-        }
-
-        } while(ancora);
-    }
+    public Giocatore(String nomeGiocatore, int ficheIniziali, int fichesPuntata) {
+		setNomeGiocatore(nomeGiocatore);
+		setFicheIniziali(ficheIniziali);
+		setFichesPuntata(fichesPuntata);
+	}
 
 
-    public int getFichesPuntata() {
-        return fichesPuntata;
-    }
+	public String getNomeGiocatore() {
+		return nomeGiocatore;
+	}
 
 
+	public void setNomeGiocatore(String nomeGiocatore) {
+		this.nomeGiocatore = nomeGiocatore;
+	}
+
+
+	public int getFicheIniziali() {
+		return ficheIniziali;
+	}
+
+
+	public void setFicheIniziali(int ficheIniziali) {
+		this.ficheIniziali = ficheIniziali;
+	}
+
+
+	public int getFichesPuntata() {
+		return fichesPuntata;
+	}
+
+
+	public void setFichesPuntata(int fichesPuntata) {
+		this.fichesPuntata = fichesPuntata;
+	}
+
+    
 
 
 
--- RouletteTest.java
+++ RouletteTest.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+
 public class RouletteTest{
 
-public static void main(String[] args) {
+public static void main(String[] args) throws IOException, InterruptedException {
     Roulette myRoulette = new Roulette();
     myRoulette.play(); 
 
